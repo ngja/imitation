3ab6638e93abef51a9b05169738697c33457a055
hansol <wkdqhrh00@naver.com>
2024-02-17 23:22:43 +0900

    github right sidebar 비슷하게 copy

--- src/components/github/sidebar/GithubLeftSidebar.tsx
+++ src/components/github/sidebar/GithubLeftSidebar.tsx
@@ -1,22 +1,24 @@
 import {Component, For} from "solid-js";
 import {repo} from "../../../db/repo"
 import GithubSidebarRepository from "./GithubSidebarRepository";
+import {Icon} from "@iconify-icon/solid";
 
 const GithubLeftSidebar: Component = () => {
-
-
-
-
   return (
     <div class="mx-6 mt-3">
-      <div>
+      <div class="px-2 pt-6">
         <div>
-          <h2>
-            Top Repositories
-            <button>New</button>
+          <h2 class="flex justify-between">
+            <span class="font-bold text-gray-500">
+              Top Repositories
+            </span>
+            <button class="h-7 px-2 border border-gray-500 bg-green-700 rounded-md text-white">
+              <span class="mr-1"><Icon icon="grommet-icons:book" /></span>
+              <span>New</span>
+            </button>
           </h2>
-          <div>
-            <input class="border-2" placeholder="Find a repository..."/>
+          <div class="mt-2 mb-4">
+            <input class="border-2 w-full py-0.5 px-2 rounded-md bg-github-light-default-header" placeholder="Find a repository..."/>
           </div>
           <div>
             <ul>
@@ -30,10 +32,10 @@ const GithubLeftSidebar: Component = () => {
             </ul>
           </div>
         </div>
-        <div>
-          <h2>Recent activity</h2>
-          <div>
-            <p>
+        <div class="mt-6">
+          <h2 class="font-bold text-gray-500">Recent activity</h2>
+          <div class="mt-3 p-3 border border-dashed rounded-md">
+            <p class="text-gray-400 text-xs">
               When you take actions across GitHub, we'll provide links to that activity here.
             </p>
           </div>
--- src/components/github/sidebar/GithubSidebarRepository.tsx
+++ src/components/github/sidebar/GithubSidebarRepository.tsx
@@ -2,12 +2,14 @@ import {Component} from "solid-js";
 
 const GithubSidebarRepository: Component<{link: string, name: string}> = (props) => {
   return (
-    <div class="flex">
-      <a href={props.link}>
-        <img src="https://avatars.githubusercontent.com/u/98628227?s=16&v=4" alt="avatar"/>
+    <div class="flex mt-1.5 leading-normal">
+      <a class="flex justify-center items-center mr-1" href={props.link}>
+        <img class="rounded-xl" src="https://avatars.githubusercontent.com/u/98628227?s=16&v=4" alt="avatar"/>
       </a>
       <div>
-        {props.name}
+        <a class="hover:underline" href={props.link}>
+          <span class="text-sm">{props.name}</span>
+        </a>
       </div>
     </div>
   )
